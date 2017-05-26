58eb0724b5409e82541d060c7cc0441b35cb38f9
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-26 13:34:46 +0200

    Added connexion to database Evalsimplon with PDO

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,6 +1,6 @@
 <?php
   try{
-	  $cnx = new PDO("mysql:host=localhost;dbname=Evalsimplon", "root", "");
+	  $cnx = new PDO("mysql:host=localhost;dbname=Evalsimplon", "root", "codeurKiFFeur");
   }
   catch (Exception $e){
   	die('Erreur : ' . $e->getMessage());
