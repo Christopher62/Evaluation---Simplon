6fe3cdfbc7c5da599f5eea81fc2ec951cfba272e
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-26 11:06:50 +0200

    Added navbar without link (test)

--- chris.php
+++ chris.php
@@ -8,6 +8,29 @@
     <link rel="stylesheet" href="style/css/gregory.css">
   </head>
   <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+          <li class="active" styyle="margin-top:0"><a href="index.php">Home</a></li>
+          <li><a href="">...</a></li>
+          <li><a href="">...</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>Sign Up</li>
+          <li>Log In</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
     <script src="https://use.fontawesome.com/8d66db2fbe.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
