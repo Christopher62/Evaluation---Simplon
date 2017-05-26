e17bce43c38db9e66b76128910afed0332545a0f
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-26 09:47:11 +0200

    Initial Commit

---
+++ chris.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Evaluation - Simplon</title>
+    <link rel="stylesheet" href="bootstrap-css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/css/christopher.css">
+    <link rel="stylesheet" href="style/css/gregory.css">
+  </head>
+  <body>
+    <script src="https://use.fontawesome.com/8d66db2fbe.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/christopher.js"></script>
+  </body>
+</html>
