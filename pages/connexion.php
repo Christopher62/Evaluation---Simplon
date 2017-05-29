9fc6e95a0ec8688147d8745b7355c0f178c56b0b
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-29 13:41:15 +0200

    Added link in navbar + inscripiton and connexion files

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,8 +1,26 @@
-<?php
-  try{
-	  $cnx = new PDO("mysql:host=localhost;dbname=Evalsimplon", "root", "codeurKiFFeur");
-  }
-  catch (Exception $e){
-  	die('Erreur : ' . $e->getMessage());
-  }
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion</title>
+    <link rel="stylesheet" href="../bootstrap-css/bootstrap.min.css">
+	</head>
+	<body>
+		<div align="center">
+			<h2>Connexion</h2>
+			<form class="well" method="POST" action="">
+        <div class="form-group">
+				  <label for="mailconnect">Mail :</label>
+				  <input class="form-control" type="email" name="mailconnect" placeholder="Mail">
+        </div>
+        <div class="form-group">
+				  <label for="mdpconnect">Mot de passe :</label>
+          <input class="form-control" type="password" name="mdpconnect" placeholder="Mot de passe">
+        </div>
+				<input type="submit" name="forminscription" value="Connexion">
+			</form>
+      Pas de compte ? <a href="inscription.php">Inscrivez-vous !</a>
+		</div>
+    <script src="../js/bootstrap.js"></script>
+	</body>
+</html>
