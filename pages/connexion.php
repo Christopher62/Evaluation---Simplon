83f529dfb3ccc2d79269d527519d7bc91023b2e9
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-29 19:53:07 +0200

    Added style to form of inscription and connexion

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,3 +1,9 @@
+<?php
+
+// futur code php qui vérifiera que l'utilisateur existe
+
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,18 +14,27 @@
 	<body>
 		<div align="center">
 			<h2>Connexion</h2>
-			<form class="well" method="POST" action="">
-        <div class="form-group">
-				  <label for="mailconnect">Mail :</label>
-				  <input class="form-control" type="email" id="mailconnect" name="mailconnect" placeholder="Mail">
+      <div class="container-fluid">
+        <div class="row">
+    			<form class="well col-md-offset-3 col-md-6" method="POST" action="">
+            <div class="form-group">
+    				  <label for="mailconnect">Mail :</label>
+    				  <input class="form-control" type="email" id="mailconnect" name="mailconnect" placeholder="Mail">
+            </div>
+            <div class="form-group">
+    				  <label for="mdpconnect">Mot de passe :</label>
+              <input class="form-control" type="password" id="mdpconnect" name="mdpconnect" placeholder="Mot de passe">
+            </div>
+    				<input type="submit" name="forminscription" value="Connexion">
+    			</form>
         </div>
-        <div class="form-group">
-				  <label for="mdpconnect">Mot de passe :</label>
-          <input class="form-control" type="password" id="mdpconnect" name="mdpconnect" placeholder="Mot de passe">
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <!-- futur code php pour prévenir des erreurs lors de la connexion (mauvais mail / mot de passe) -->
+          Pas encore de compte ? <a href="inscription.php">Inscrivez-vous</a> !
         </div>
-				<input type="submit" name="forminscription" value="Connexion">
-			</form>
-      Pas de compte ? <a href="inscription.php">Inscrivez-vous !</a>
+      </div>
 		</div>
     <script src="../js/bootstrap.js"></script>
 	</body>
--- pages/inscription.php
+++ pages/inscription.php
@@ -1,3 +1,9 @@
+<?php
+
+// futur code php qui va vérifier les données de l'utilisateur !
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,30 +14,38 @@
 	<body>
 		<div align="center">
 			<h2>Inscription</h2>
-			<form class="well" method="POST" action="">
-        <div class="form-group">
-          <label for="pseudo">Pseudo :</label>
-				  <input class="form-control" type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="">
-        </div>
-        <div class="form-group">
-				  <label for="mail">Mail :</label>
-				  <input class="form-control" type="email" placeholder="Votre mail" id="mail" name="mail" value="">
-        </div>
-        <div class="form-group">
-          <label for="mail2">Confirmation du mail :</label>
-				  <input class="form-control" type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="">
-        </div>
-        <div class="form-group">
-          <label for="mdp">Mot de passe :</label>
-				  <input class="form-control" type="password" placeholder="Votre mot de passe" id="mdp" name="mdp">
-        </div>
-        <div class="form-group">
-				  <label for="mdp2">Confirmation du mot de passe :</label>
-          <input class="form-control" type="password" placeholder="Confirmation votre mdp" id="mdp2" name="mdp2">
-        </div>
-				<input type="submit" name="forminscription" value="Inscription">
-			</form>
-		</div>
-    <script src="../js/bootstrap.js"></script>
+			<div class="container-fluid">
+				<div class="row">
+					<form class="well col-md-offset-3 col-md-6" method="POST" action="">
+		        <div class="form-group">
+		          <label for="pseudo">Pseudo :</label>
+						  <input class="form-control" type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>">
+		        </div>
+		        <div class="form-group">
+						  <label for="mail">Mail :</label>
+						  <input class="form-control" type="email" placeholder="Votre mail" id="mail" name="mail" value="<?php if(isset($mail)) { echo $mail; } ?>">
+		        </div>
+		        <div class="form-group">
+		          <label for="mail2">Confirmation du mail :</label>
+						  <input class="form-control" type="email" placeholder="Confirmez votre mail" id="mail2" name="mail2" value="<?php if(isset($mail2)) { echo $mail2; } ?>">
+		        </div>
+		        <div class="form-group">
+		          <label for="mdp">Mot de passe :</label>
+						  <input class="form-control" type="password" placeholder="Votre mot de passe" id="mdp" name="mdp">
+		        </div>
+		        <div class="form-group">
+						  <label for="mdp2">Confirmation du mot de passe :</label>
+		          <input class="form-control" type="password" placeholder="Confirmation mot de passe" id="mdp2" name="mdp2">
+		        </div>
+						<input type="submit" name="forminscription" value="Inscription">
+					</form>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<!-- futur code php pour prévenir des erreurs lors de l'inscription -->
+					Vous avez déjà un compte ? <a href="connexion.php">Connectez-vous</a> !
+				</div>
+			</div>
 	</body>
 </html>
