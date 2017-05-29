bf5d93418202a0e3a1e02ee1e9ff5b775df0a920
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-29 15:12:50 +0200

    Added pages folder without back to master branch

---
+++ pages/profil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profil</title>
+	</head>
+	<body>
+		<div align="center">
+			<h2>Profil de </h2>
+
+			Pseudo =
+
+			Mail =
+
+
+			<a href="editionprofil.php">Editer mon profil</a>
+			<a href="deconnexion.php">Se déconnecter</a>
+
+		</div>
+	</body>
+</html>
