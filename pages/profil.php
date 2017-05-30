7d46cdd4021bce9dbc200ecc79bc36a27665e658
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-30 16:10:10 +0200

    Security : profil modification only if user -> logged in

--- pages/profil.php
+++ pages/profil.php
@@ -3,12 +3,16 @@ session_start();
 
 $cnx = new PDO('mysql:host=localhost;dbname=Evalsimplon', 'root', 'codeurKiFFeur');
 
-if(isset($_GET["id"]) AND $_GET["id"] > 0)
+if(isset($_GET["id"]) AND $_GET["id"] >= 0)
 {
 	$getid = intval($_GET["id"]);
 	$requser = $cnx->prepare("SELECT * FROM membres WHERE id = ?");
 	$requser->execute(array($getid));
 	$userinfo = $requser->fetch();
+	$requeserexist = $requser->rowCount();
+	if($requeserexist == 0){
+		header("Location: 404.php");
+	}
 }
 ?>
 <!DOCTYPE html>
@@ -23,6 +27,13 @@ if(isset($_GET["id"]) AND $_GET["id"] > 0)
 	<body>
 		<div align="center">
 			<h2>Profil de <?php echo $userinfo["pseudo"]; ?></h2>
+			<?php if(!empty($userinfo['avatar']))
+			{
+			?>
+			<img src="../img/membres/avatars/default.jpg" width="150">
+			<?php
+			}
+			?>
 			<table class="table table-bordered profil-table">
 				<tr>
 					<th>Pseudo</th>
@@ -33,8 +44,19 @@ if(isset($_GET["id"]) AND $_GET["id"] > 0)
 					<td><?php echo $userinfo["pseudo"]; ?></td>
 					<td><?php echo $userinfo["mail"]; ?></td>
 					<td>
-						<input onclick=window.location.href="editionprofil.php" type="button" class="btn btn-primary" name="" value="Editer mon profil">
+						<?php
+						if(isset($_SESSION["id"]) AND $userinfo["id"] == $_SESSION["id"])
+						{
+						?>
+						<input onclick=window.location.href="<?php echo "editionprofil.php?id=".$_SESSION["id"]; ?>" type="button" class="btn btn-primary" name="" value="Editer mon profil">
 						<input onclick=window.location.href="deconnexion.php" type="button" class="btn btn-danger" name="" value="Déconnexion">
+						<?php
+						}
+						else
+						{
+							echo "...";
+						}
+						?>
 					</td>
 				</tr>
 			</table>
