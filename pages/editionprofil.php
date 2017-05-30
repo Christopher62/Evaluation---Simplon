82e102622434b07aab62b0b8b5ed5a3ff4a25ebc
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-30 19:16:20 +0200

    Fix style of the form and fix the condition for avoid the 404 error

--- pages/editionprofil.php
+++ pages/editionprofil.php
@@ -12,30 +12,49 @@
 	<body>
 		<div align="center">
 			<h2>Edition de mon profil</h2>
-			<form class="well col-md-offset-4 col-md-4" method="POST" action="" enctype="multipart/form-data">
-        <div class="form-group">
-				  <label for="newpseudo">Pseudo :</label>
-				  <input class="form-control" id="newpseudo" type="text" name="newpseudo" placeholder="Pseudo" value="">
-        </div>
-        <div class="form-group">
-  				<label for="newmail">Mail :</label>
-  				<input class="form-control" id="newmail" type="email" name="newmail" placeholder="Mail" value="">
-        </div>
-        <div class="form-group">
-  				<label for="newmdp1">Mot de passe :</label>
-  				<input class="form-control" id="newmdp1" type="password" name="newmdp1" placeholder="Mot de passe">
-        </div>
-        <div class="form-group">
-  				<label for="newmdp2">Confirmation du mot de passe :</label>
-  				<input class="form-control" id="newmdp2" type="password" name="newmdp2" placeholder="Confirmation du mot de passe">
-        </div>
-        <div class="form-group">
-          <label for="avatar">Avatar :</label>
-				  <input id="avatar" type="file" name="avatar">
-        </div>
-				<input onclick=window.location.href="<?php echo "profil.php?id=".$_SESSION["id"]; ?>" type="button" class="btn btn-warning" name="submit" value="Retour vers mon profil">
-				<input type="submit" class="btn btn-success" name="submit" value="Mettre à jour mon profil">
-			</form>
+			<div class="container-fluid">
+				<div class="row">
+					<form class="well col-md-offset-4 col-md-4" method="POST" action="" enctype="multipart/form-data">
+		        <div class="form-group">
+						  <label for="newpseudo">Pseudo :</label>
+						  <input class="form-control" id="newpseudo" type="text" name="newpseudo" placeholder="Pseudo" value="">
+		        </div>
+		        <div class="form-group">
+		  				<label for="newmail">Mail :</label>
+		  				<input class="form-control" id="newmail" type="email" name="newmail" placeholder="Mail" value="">
+		        </div>
+		        <div class="form-group">
+		  				<label for="newmdp1">Mot de passe :</label>
+		  				<input class="form-control" id="newmdp1" type="password" name="newmdp1" placeholder="Mot de passe">
+		        </div>
+		        <div class="form-group">
+		  				<label for="newmdp2">Confirmation du mot de passe :</label>
+		  				<input class="form-control" id="newmdp2" type="password" name="newmdp2" placeholder="Confirmation du mot de passe">
+		        </div>
+		        <div class="form-group">
+		          <label for="avatar">Avatar :</label>
+						  <input id="avatar" type="file" name="avatar">
+		        </div>
+						<input onclick=window.location.href="<?php echo "profil.php?id=".$_SESSION["id"]; ?>" type="button" class="btn btn-warning" name="submit" value="Retour vers mon profil">
+						<input type="submit" class="btn btn-success" name="submit" value="Mettre à jour mon profil">
+					</form>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<?php
+					if(isset($msg)){
+						echo '<font color="red">'.$msg.'</font>';
+					 }
+					?>
+					<?php
+					if(isset($message)){
+						echo '<font color="green">'.$message.'</font>';
+					 }
+					?>
+				</div>
+			</div>
+			<?php if(isset($_POST["submit"]) && isset($_SESSION['id'])) { header("Location: profil.php?id=".$_SESSION["id"]); } ?>
 		</div>
     <script src="../js/bootstrap.js"></script>
 	</body>
