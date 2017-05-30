af0fa878f35bc090fca4a0e233eb351e7b2953e1
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-30 14:11:33 +0200

    Added redirect to profil in connexion file / Added data to profil (user + mail)

--- pages/connexion.php
+++ pages/connexion.php
@@ -18,7 +18,7 @@ if(isset($_POST["formconnexion"]))
 			$_SESSION["id"] = $userinfo["id"];
 			$_SESSION["pseudo"] = $userinfo["pseudo"];
 			$_SESSION["mail"] = $userinfo["mail"];
-
+			header("Location: profil.php?id=".$_SESSION["id"]);
 		}
 		else
 		{
--- pages/profil.php
+++ pages/profil.php
@@ -1,15 +1,28 @@
+<?php
+session_start();
+
+$cnx = new PDO('mysql:host=localhost;dbname=Evalsimplon', 'root', 'codeurKiFFeur');
+
+if(isset($_GET["id"]) AND $_GET["id"] > 0)
+{
+	$getid = intval($_GET["id"]);
+	$requser = $cnx->prepare("SELECT * FROM membres WHERE id = ?");
+	$requser->execute(array($getid));
+	$userinfo = $requser->fetch();
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Profil</title>
+		<title>Profil de <?php echo $userinfo["pseudo"]; ?></title>
 		<link rel="stylesheet" href="../bootstrap-css/bootstrap.min.css">
 		<link rel="stylesheet" href="../style/css/christopher.css">
 	</head>
 	<body>
 		<div align="center">
-			<h2>Profil de <!-- futur code php qui indiquera le pseudo de l'utilisateur --> </h2>
+			<h2>Profil de <?php echo $userinfo["pseudo"]; ?></h2>
 			<table class="table table-bordered profil-table">
 				<tr>
 					<th>Pseudo</th>
@@ -17,8 +30,8 @@
 					<th>Actions</th>
 				</tr>
 				<tr>
-					<td> <!-- futur code php qui indiquera le pseudo de l'utilisateur --></td>
-					<td> <!-- futur code php qui indiquera l'email de l'utilisateur --></td>
+					<td><?php echo $userinfo["pseudo"]; ?></td>
+					<td><?php echo $userinfo["mail"]; ?></td>
 					<td>
 						<input onclick=window.location.href="editionprofil.php" type="button" class="btn btn-primary" name="" value="Editer mon profil">
 						<input onclick=window.location.href="deconnexion.php" type="button" class="btn btn-danger" name="" value="Déconnexion">
