957c6ac8bef0e6345fe0c31c276d4222c8c933ae
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-30 16:12:16 +0200

    Added 404 file if id not found (test)

---
+++ pages/404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Not found</title>
+    <link rel="stylesheet" href="../bootstrap-css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style/css/christopher.css">
+  </head>
+  <body>
+    <div align="center">
+      <h1>404 not found</h1>
+      <table class="table table-bordered notfound-table">
+        <tr>
+          <th>Actions</th>
+        </tr>
+        <tr>
+          <td>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
