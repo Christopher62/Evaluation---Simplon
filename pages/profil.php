b0ab6737b910cae479cfc3995a70d5ba5d593839
Christopher62 <christopher.dachicourt@gmail.com>
2017-05-30 13:31:22 +0200

    Added link bootstrap for style

--- pages/profil.php
+++ pages/profil.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Profil</title>
+		<link rel="stylesheet" href="../bootstrap-css/bootstrap.min.css">
+		<link rel="stylesheet" href="../style/css/christopher.css">
 	</head>
 	<body>
 		<div align="center">
