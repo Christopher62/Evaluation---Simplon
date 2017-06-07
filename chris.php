2aead8d770677d5a7f490c261d6c6f66880e7141
Christopher62 <christopher.dachicourt@gmail.com>
2017-06-07 10:30:12 +0200

    Added link for Articles

--- chris.php
+++ chris.php
@@ -37,6 +37,8 @@ $cnx = new PDO("mysql:host=localhost;dbname=Evalsimplon", "root", "codeurKiFFeur
           <li class="dropdown">
             <a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span style="text-transform:uppercase"><?php echo $_SESSION["pseudo"]; ?></span>&nbsp;<span class="caret"></span></a>
             <ul class="dropdown-menu">
+              <li><a href="">Gestion des Articles</a></li>
+              <li class="divider"></li>
               <li><a href="pages/profil.php?id=<?php echo $_SESSION["id"]; ?>">Mon Profil</a></li>
               <li><a href="pages/editionprofil.php?id=<?php echo $_SESSION["id"]; ?>">Paramètres</a></li>
               <li class="divider"></li>
